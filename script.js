7ce2442c98d6dd251b8d194377deb2da9e05ff86
unknown <aw2898906@gmail.com>
2026-06-23 11:55:02 -0700

    Initial commit - portfolio project

---
+++ script.js
@@ -0,0 +1,9 @@
+let navlinks = document.getElementById("navlinks");
+let menu_icon = document.getElementById("menu-icon");
+
+menu_icon.addEventListener("click", () => {
+    navlinks.classList.toggle("active");
+});
+
+
+
