8435f120fbce0de362b041f37de9cef57fcd2a26
unknown <aw2898906@gmail.com>
2026-07-21 08:59:41 -0700

    update project

--- script.js
+++ script.js
@@ -115,3 +115,108 @@ const cardObserver = new IntersectionObserver((entries)=>{
 document.querySelectorAll(".desing-card").forEach(card=>{
     cardObserver.observe(card);
 });
+
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll(".new-section, .creative-box").forEach((el) => {
+  observer4.observe(el);
+});
+
+const observerAction = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show2");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+const animationItems = document.querySelectorAll(
+  ".inovation-heading, .brand-box, .action-sec"
+);
+
+animationItems.forEach((item) => {
+  observerAction.observe(item);
+});
+
+
+const observer6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show3");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+const businessHeading = document.querySelector(".bussnies-heading");
+observer6.observe(businessHeading);
+
+
+// Classes add karo
+document.querySelector(".mini-title").classList.add("animate-skill");
+document.querySelector(".skills-heading").classList.add("animate-skill");
+document.querySelector(".skills-para").classList.add("animate-skill");
+document.querySelector(".small-line").classList.add("animate-skill");
+
+document.querySelectorAll(".stat-card").forEach(card => {
+  card.classList.add("animate-skill");
+});
+
+document.querySelector(".learning-box").classList.add("animate-skill");
+
+document.querySelectorAll(".skill-item").forEach(item => {
+  item.classList.add("animate-skill");
+});
+
+// Observer
+const observer7 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show4");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll(".animate-skill").forEach((item) => {
+  observer7.observe(item);
+});
+
+// Animate class add karo
+document.querySelector(".faq-badge").classList.add("animate-faq");
+document.querySelector(".faq-left h2").classList.add("animate-faq");
+document.querySelector(".underline").classList.add("animate-faq");
+document.querySelector(".faq-left > p").classList.add("animate-faq");
+document.querySelector(".faq-illustration").classList.add("animate-faq");
+document.querySelector(".contact-box").classList.add("animate-faq");
+
+document.querySelectorAll(".faq-right details").forEach((item) => {
+  item.classList.add("animate-faq");
+});
+
+// Observer
+const observer8 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show5");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll(".animate-faq").forEach((item) => {
+  observer8.observe(item);
+});
